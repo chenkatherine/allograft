989a0c3769efaafd9f18f8fbcc86cb52291cf7c0
Katherine Chen <chenkatherine@uchicago.edu>
2025-05-23 10:41:46 -0500

    Added explicit download of the model. Adjusted some hyperparameters.

--- uni_for_git.py
+++ uni_for_git.py
@@ -19,28 +19,54 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Use the get_encoder function to load the UNI2-h model
 from huggingface_hub import login
 
-# login with user access token (removed)
+# Login with user access token
+# removed for git push
+
+# Downloading weights and creating model
+import timm
+from timm.data import resolve_data_config
+from timm.data.transforms_factory import create_transform
+
+# pretrained=True needed to load UNI2-h weights (and download weights for the first time)
+timm_kwargs = {
+            'img_size': 224, 
+            'patch_size': 14, 
+            'depth': 24,
+            'num_heads': 24,
+            'init_values': 1e-5, 
+            'embed_dim': 1536,
+            'mlp_ratio': 2.66667*2,
+            'num_classes': 0, 
+            'no_embed_class': True,
+            'mlp_layer': timm.layers.SwiGLUPacked, 
+            'act_layer': torch.nn.SiLU, 
+            'reg_tokens': 8, 
+            'dynamic_img_size': True
+        }
+model = timm.create_model("hf-hub:MahmoodLab/UNI2-h", pretrained=True, **timm_kwargs)
+transform = create_transform(**resolve_data_config(model.pretrained_cfg, model=model))
+model.eval()
 
 # Use the get_encoder function which properly sets up all the required parameters
 model, transform = get_encoder(enc_name='uni2-h', device=device)
 
-
 # ROI feature extraction
 from uni.downstream.extract_patch_features import extract_patch_features_from_dataloader
 
-# get path to example data
+# Get path to saved data
 dataroot = "HE_IMAGES"
 
-# also set the image max to none or else it throws a DecompressionBomb Error
+# Set the image max to 'None' or to avoid a DecompressionBomb Error
 Image.MAX_IMAGE_PIXELS = None
 
-# create some image folder datasets for train/test and their data laoders
+# Create some image folder datasets for train/test and their data laoders
 train_dataset = torchvision.datasets.ImageFolder(j_(dataroot, 'train'), transform=transform)
 test_dataset = torchvision.datasets.ImageFolder(j_(dataroot, 'test'), transform=transform)
-train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=False)
-test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=4, shuffle=False)
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=False)
+test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=16, shuffle=False)
 
-# extract patch features from the train and test datasets (returns dictionary of embeddings and labels)
+# Extract patch features from the train and test datasets (returns dictionary 
+# of embeddings and labels)
 train_features = extract_patch_features_from_dataloader(model, train_dataloader)
 test_features = extract_patch_features_from_dataloader(model, test_dataloader)
 
@@ -58,17 +84,17 @@ fewshot_episodes, fewshot_dump = eval_fewshot(
     train_labels = train_labels,
     test_feats = test_feats,
     test_labels = test_labels,
-    n_iter = 500, # draw 500 few-shot episodes
-    n_way = 2, # use all class examples
-    n_shot = 4, # 4 examples per class (as we don't have that many)
+    n_iter = 1000, # Originally 500
+    n_way = 3, # Originally 2
+    n_shot = 8, # Originally 4
     n_query = test_feats.shape[0], # evaluate on all test samples
-    center_feats = True,
+    center_feats = False, # Originally True
     normalize_feats = True,
-    average_feats = True,
+    average_feats = False, # Originally True
 )
 
-# how well we did picking 4 random examples per class
+# Evaluation
 print(fewshot_episodes)
 
-# summary
+# Summary
 print(fewshot_dump)
